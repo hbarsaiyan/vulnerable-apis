37e5b77bd5ed9697e10b650b27c6ebd3f89b3080
ayushaga14 <ayush@akto.io>
2023-07-14 14:45:07 +0530

    add allowed hosts

--- vulnerableapis/settings.py
+++ vulnerableapis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2yy0t497vdj49lmb#o-3^2c@h#&+qn#05uml@kpt%)xb=--x6g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
