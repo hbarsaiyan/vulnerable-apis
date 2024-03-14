41ae2bf206d330f9f2ca65625160dec8419d408c
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2024-03-14 15:20:18 +0530

    change auth check from middleware to decorator
    since we only want to apply auth check to fetch_sample_data
    
    (cherry picked from commit 8af2cb2172588c1623de8432e9604a89f796bd31)

---
+++ mockvulnerableapis/decorator.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from django.http import HttpResponse
+
+def authentication_token_required(view_function):
+    @wraps(view_function)
+    def wrap(request, *args, **kwargs):
+        # exempt these paths from auth check
+        bypass_paths = ['/login']
+        if any(path in request.path for path in bypass_paths):
+            return view_function(request, *args, **kwargs)
+        if 'HTTP_AUTHORIZATION' not in request.META:
+            return HttpResponse('Unauthorized', status=401)
+        return view_function(request, *args, **kwargs)
+    return wrap
