e9fe525b4e9e621c79b95cae628ab68e0b9ab85a
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2024-03-16 12:56:26 +0530

    remove bypass_paths
    not needed

--- mockvulnerableapis/decorator.py
+++ mockvulnerableapis/decorator.py
@@ -4,10 +4,6 @@ from django.http import HttpResponse
 def authorization_check(view_function):
     @wraps(view_function)
     def wrap(request, *args, **kwargs):
-        # exempt these paths from auth check
-        bypass_paths = ['/login']
-        if any(path in request.path for path in bypass_paths):
-            return view_function(request, *args, **kwargs)
         if 'HTTP_ACCESS_TOKEN' not in request.META:
             return HttpResponse('Unauthorized', status=401)
         return view_function(request, *args, **kwargs)
