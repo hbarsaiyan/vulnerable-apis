e55e26e1c5aea38f0c272394bb033070e28e25c5
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2024-03-22 22:49:44 +0530

    feat: handle multiple nodes and simulate response time delays

--- mockvulnerableapis/models.py
+++ mockvulnerableapis/models.py
@@ -3,5 +3,11 @@ from django.db import models
 
 
 class SampleData(models.Model):
-    url = models.CharField(max_length=100, unique=True)
+    url = models.CharField(max_length=100)
+    node_id = models.CharField(max_length=50, default="x1")
     data = models.TextField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['url', 'node_id'], name='unique_url_node_id')
+        ]
--- mockvulnerableapis/views.py
+++ mockvulnerableapis/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .decorator import authorization_check
+import asyncio
+from asgiref.sync import async_to_sync
 import json
 import os
 import copy
@@ -15,37 +17,40 @@ from types import SimpleNamespace
 def add_sample_data(request):
     url = request.data["url"]
     data = request.data["data"]
+    node_id = request.data.get("node_id", "x1")
     try:
-        if SampleData.objects.filter(url=url).exists():
-            SampleData.objects.update(data=json.dumps(data))
-        else:
-            SampleData.objects.create(url=url, data=json.dumps(data))
-
+        SampleData.objects.update_or_create(url=url, node_id=node_id, defaults = {"data": json.dumps(data)})
         return HttpResponse(json.dumps(request.data), status=200)
     except Exception as e:
         return HttpResponse("Error Inserting Sample Data".format(e), status=500)
 
+@async_to_sync
+async def resp_delay(api_resp, delay=0):
+    await asyncio.sleep(delay)
+    return api_resp
 
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'TRACE', 'TRACK', 'TestMethod'])
 @authorization_check
 def fetch_sample_data(request):
     url = request.path
+    node_id = request.headers.get('x-akto-node', "x1")
     print(url)
     try:
-        sample_data = SampleData.objects.get(url=url)
+        sample_data = SampleData.objects.get(url=url, node_id=node_id)
         print("sample_data_url")
         print(sample_data.url)
         sample_data_json = json.loads(sample_data.data)
         resp = sample_data_json["responsePayload"]
         status_code = sample_data_json["statusCode"]
         headers = sample_data_json["responseHeaders"]
+        median_response_time = sample_data_json["medianResponseTime"]
 
         api_resp = HttpResponse(json.dumps(resp), status=status_code)
 
         for key, value in headers.items():
             api_resp[key] = value
 
-        return api_resp
+        return resp_delay(api_resp, median_response_time/1000)
     except Exception as e:
         return HttpResponse("Error Executing Request".format(e), status=500)
 
@@ -53,6 +58,33 @@ def fetch_sample_data(request):
 @api_view(['GET'])
 def metrics(request):
     return HttpResponse(json.dumps("{}"), status=200)
+
+def fetch_data(testData, id):
+    resp = testData.responsePayload
+    headers = testData.responseHeaders
+    try:
+        resp = vars(testData.responsePayload)
+        for key in resp:
+            try:
+                resp[key] = vars(resp[key])
+            except Exception as e:
+                pass
+    except Exception as e:
+        print("error loading responsePayload " + id + " " + str(e))
+    try:
+        headers = vars(testData.responseHeaders)
+    except Exception as e:
+        print("error loading responseHeaders " + id + " " + str(e))
+
+    data = {
+        "method": testData.method,
+        "responsePayload": resp,
+        "statusCode": testData.statusCode,
+        "responseHeaders": headers,
+        "medianResponseTime": getattr(testData, 'medianResponseTime', 0)
+    }
+
+    return data, testData.url
 
 @api_view(['POST'])
 def insert_data(request):
@@ -64,34 +96,13 @@ def insert_data(request):
 
     for obj in testData:
         try:
-            resp = obj.testData.responsePayload
-            headers = obj.testData.responseHeaders
-            try:
-                resp = vars(obj.testData.responsePayload)
-                for key in resp:
-                    try:
-                        resp[key] = vars(resp[key])
-                    except Exception as e:
-                        pass
-            except Exception as e:
-                print("error loading responsePayload " + obj.id + " " + str(e))
-            try:
-                headers = vars(obj.testData.responseHeaders)
-            except Exception as e:
-                print("error loading responseHeaders " + obj.id + " " + str(e))
-
-            data = {
-                "method": obj.testData.method,
-                "responsePayload": resp,
-                "statusCode": obj.testData.statusCode,
-                "responseHeaders": headers
-            }
-
-            url = obj.testData.url
-            if SampleData.objects.filter(url=url).exists():
-                SampleData.objects.filter(url=url).update(data=json.dumps(data))
+            if hasattr(obj.testData, 'method'):
+                data, url = fetch_data(obj.testData, obj.id)
+                SampleData.objects.update_or_create(url=url, node_id="x1", defaults = {"data": json.dumps(data)})
             else:
-                SampleData.objects.create(url=url, data=json.dumps(data))
+                for node_id, nodeTestData in obj.testData.__dict__.items():
+                    node_data, node_url = fetch_data(nodeTestData, obj.id)
+                    SampleData.objects.update_or_create(url=node_url, node_id=node_id, defaults = {"data": json.dumps(node_data)})
 
         except Exception as e:
             print("error inserting data " + obj.id + " " + str(e))
