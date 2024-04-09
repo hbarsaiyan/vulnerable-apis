13901386f1932aa46369cd2aff8126daa56e44b9
Harsh Barsaiyan <hbarsaiyan@gmail.com>
2024-04-09 15:47:55 +0530

    allow access token for now

--- mockvulnerableapis/decorator.py
+++ mockvulnerableapis/decorator.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 def authorization_check(view_function):
     @wraps(view_function)
     def wrap(request, *args, **kwargs):
-        if 'HTTP_AUTHORIZATION' not in request.META:
+        if ('HTTP_AUTHORIZATION' in request.META or 'HTTP_ACCESS_TOKEN' in request.META):
+            return view_function(request, *args, **kwargs)
+        else:
             return HttpResponse('Unauthorized', status=401)
-        return view_function(request, *args, **kwargs)
     return wrap
